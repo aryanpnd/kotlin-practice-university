30602833e68bfd62e5760f7f8f5c4da838be0a0e
aryanpnd <captainaryan9870@gmail.com>
2024-08-22 14:13:32 +0530

    days 4

---
+++ codes/day4/operators/assignmentOperators.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var a = 10
+    var b = 20
+
+    a = a+b
+    println("value of a after (a = a+b) : $a")
+
+    a = a-b
+    println("value of a after (a = a-b) : $a")
+
+    a = a*b
+    println("value of a after (a = a*b) : $a")
+
+    a = a/b
+    println("value of a after (a = a/b) : $a")
+
+    a = a%b
+    println("value of a after (a = a%b) : $a")
+}
---
+++ codes/day4/operators/unaryOperators.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var a = 10
+
+    println("value of a  after +a : ${+a}")
+    println("value of a  after -a : ${-a}")
+    println("value of a  after ++a : ${++a}")
+    println("value of a  after --a : ${--a}")
+    println("value of a  after !a : ${!a}")
+}
